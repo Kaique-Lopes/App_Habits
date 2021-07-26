c5e30c760e9503fe4fd81ea88d04e00a60307697
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-07-26 18:48:05 -0300

    Initial commit

---
+++ App_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_HabitsApp.swift
+//  App_Habits
+//
+//  Created by Kaique Lopes on 26/07/21.
+//
+
+import SwiftUI
+
+@main
+struct App_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
