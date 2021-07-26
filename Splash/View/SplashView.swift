0b39b24f7dafdb8319decf9034497ab35bfb9afe
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-07-26 18:58:20 -0300

    First Commits

---
+++ Splash/View/SplashView.swift
@@ -0,0 +1,23 @@
+//
+//  SplashView.swift
+//  App_Habits
+//
+//  Created by Kaique Lopes on 26/07/21.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("Ola")
+    }
+}
+
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
+
+
