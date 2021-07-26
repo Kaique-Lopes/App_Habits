cf208f87e3bc2ff118baccd9d7f5c002b25d3e6d
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-07-26 19:00:36 -0300

    splashUIState

---
+++ Splash/Model/SplashUIState.swift
@@ -0,0 +1,15 @@
+//
+//  SplashUIState.swift
+//  App_Habits
+//
+//  Created by Kaique Lopes on 26/07/21.
+//
+
+import Foundation
+
+enum SplashUIState {
+    case loading
+    case goToSignScreen
+    case goToHomeScreen
+    case error(String)
+}
