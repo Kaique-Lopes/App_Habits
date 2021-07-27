043abc32781abeaff6350dba4af672acb1574657
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-07-26 22:26:09 -0300

    LoadingView

--- Splash/View/SplashView.swift
+++ Splash/View/SplashView.swift
@@ -14,15 +14,7 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            ZStack {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(20)
-                    .background(Color.white)
-                    .ignoresSafeArea()
-            }
+            LoadingView()
         case .goToSignScreen:
             Text("Tela Login")
         case .goToHomeScreen:
@@ -33,7 +25,19 @@ struct SplashView: View {
     }
 }
 
-
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
+        }
+    }
+}
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
         SplashView(state: .loading)
